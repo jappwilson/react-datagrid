240158cc609e3e2265bc2dcba00ef946cbdcba2b
CIQINDIA\jshekhawat <jshekhawat@spcapitaliq.com>
2017-02-01 14:55:13 +0530

    to use updated load mask

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import { findDOMNode } from 'react-dom'
 import React from 'react'
 
 var assign   = require('object-assign')
-import LoadMask from 'react-load-mask'
+import LoadMask from 'jappwilson-react-load-mask'
 var Region   = require('region')
 
 var PaginationToolbar = React.createFactory(require('./PaginationToolbar'))
